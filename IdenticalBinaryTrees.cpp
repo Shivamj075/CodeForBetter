5e410029f2547f37d9e591970fe3206a88e026fc
Shivam Jain <33578957+Shivamj075@users.noreply.github.com>
2019-12-16 02:06:23 +0530

    Create IdenticalBinaryTrees.cpp

---
+++ IdenticalBinaryTrees.cpp
@@ -0,0 +1,44 @@
+/*
+
+Given two binary trees, write a function to check if they are equal or not.
+
+Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+
+Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+
+Example :
+
+Input : 
+
+   1       1
+  / \     / \
+ 2   3   2   3
+
+Output : 
+  1 or True
+  
+  */
+  
+  /**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+bool isIdentical(TreeNode* A,TreeNode *B){
+    if(A==NULL and B==NULL)
+        return 1;
+        
+    if(A!=NULL and B!=NULL){
+        return A->val==B->val && isIdentical(A->left,B->left)&&isIdentical(A->right,B->right);
+    }
+    return 0;
+}
+int Solution::isSameTree(TreeNode* A, TreeNode* B) {
+    if(isIdentical(A,B))
+        return 1;
+    return 0;
+}
