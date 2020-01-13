64841da045db74c4148171d0495c8e96320f2ef3
Shivam Jain <33578957+Shivamj075@users.noreply.github.com>
2020-01-13 20:22:05 +0530

    Create NearestSmallerElement.cpp

---
+++ NearestSmallerElement.cpp
@@ -0,0 +1,17 @@
+vector<int> Solution::prevSmaller(vector<int> &A) {
+    stack<int> s;
+    int n = A.size();
+    vector<int> ans(n,-1);
+    for(int i=n-1;i>=0;i--){
+        if(s.empty() or A[s.top()]<=A[i])
+            s.push(i);
+        else{
+            while(!s.empty() and A[s.top()]>A[i]){
+                ans[s.top()] = A[i];
+                s.pop();
+            }
+            s.push(i);
+        }
+    }
+    return ans;
+}
