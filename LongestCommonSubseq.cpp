f3adf9f754ef0933f99c639d97901aa9bfe19056
Shivam Jain <33578957+Shivamj075@users.noreply.github.com>
2020-02-06 23:10:50 +0530

    Create LongestCommonSubseq.cpp

---
+++ LongestCommonSubseq.cpp
@@ -0,0 +1,17 @@
+int Solution::solve(string A, string B) {
+    int n = A.size(),m=B.size();
+    vector<vector<int>> lcs(n+1,vector<int>(m+1,0));
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(B[i-1]==A[j-1]){
+                lcs[i][j] = 1+lcs[i-1][j-1];
+            }
+            else{
+                lcs[i][j] = max(lcs[i-1][j],lcs[i][j-1]);
+            }
+        }
+    }
+    
+    return lcs[n][m];
+}
