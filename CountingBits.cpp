a48acb40c035d3c12a8cf3f34ff4b013ac9c0b1c
Shivam Jain <33578957+Shivamj075@users.noreply.github.com>
2020-02-10 10:27:11 +0530

    Create CountingBits.cpp

---
+++ CountingBits.cpp
@@ -0,0 +1,17 @@
+//LeetCode
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        
+        vector<int> dp(num+1,0);
+        
+        for(int i=1;i<=num;i++){
+            if(i&1)
+                dp[i] = dp[i-1]+1;
+            else
+                dp[i] = dp[i/2];
+        }
+        
+        return dp;
+    }
+};
